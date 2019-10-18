b96672a7144aab5aebf97e110a1cbc1fd50ceb2e
Huy Nguyen <huy.m.nguyen@live.com>
2019-10-17 20:04:52 -0700

    Add render methods for book card and jumbotron.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -55,4 +55,63 @@ class Home extends Component {
             image: book.volumeInfo.imageLinks.thumbnail
         }).then(() => this.getBooks());
     };
+
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col size="md-12">
+                        <Jumbotron>
+                            <h1 className="text-center">
+                                <strong>React Google Book Search</strong>
+                            </h1>
+                            <h2 className="text-center">Search for and save books of your choice.</h2>
+                        </Jumbotron>
+                    </Col>
+                    <Col size="md-12">
+                        <Card title="Book Search" icon="far fa-book">
+                            <Form
+                                handleInputChange={this.handleInputChange}
+                                handleFormSubmit={this.handleFormSubmit}
+                                q={this.state.q}
+                            />
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size="md-12">
+                        <Card title="Results">
+                            {this.state.books.length ? (
+                                <List>
+                                    {this.state.books.map(book => (
+                                        <Book
+                                            key={book.id}
+                                            title={book.volumeInfo.title}
+                                            subtitle={book.volumeInfo.subtitle}
+                                            link={book.volumeInfo.infoLink}
+                                            authors={book.volumeInfo.authors.join(", ")}
+                                            description={book.volumeInfo.description}
+                                            image={book.volumeInfo.imageLinks.thumbnail}
+                                            Button={() => (
+                                                <button
+                                                    onClick={() => this.handleBookSave(book.id)}
+                                                    className="btn btn-primary ml-2"
+                                                >
+                                                    Save
+                                                </button>
+                                            )}
+                                        />
+                                    ))}
+                                </List>
+                            ) : (
+                                <h2 className="text-center">{this.state.message}</h2>
+                            )}
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
 }
+
+export default Home;
